fc483efc79c8b5e9fbe8e2cae6fc5265477e73e5
Danielle Yasmin Dias Padilha <162368805+DanizitaDs@users.noreply.github.com>
2025-06-02 16:54:36 -0300

    atualização da aula 02/06

--- aulas/aula4/src/server.ts
+++ aulas/aula4/src/server.ts
@@ -1,14 +1,18 @@
-import express, { Application } from 'express';
-import { AppDataSource } from './database/data-source';
-import routes from './routes/user.routes';
+import express from "express"
+import routes from "./routes/user.routes"
+import produtoRoutes from "./routes/produtoRoutes"
+import { AppDataSource } from "./database/data-source"
 
-AppDataSource.initialize()
-    .then(() => {
-        const app: Application = express();
-        app.use(express.json());
+const app = express()
 
-        app.use('/api', routes);
+app.use(express.json())
+app.use('/api', routes)
+app.use('/api', produtoRoutes)
 
-        app.listen(3000, () => console.log('Server rodando na porta 3000'));
+AppDataSource.initialize().then(() => {
+    app.listen(3000, () => {
+        console.log('Server is running on http://localhost:3000')
     })
-    .catch((error) => console.log(error));
+}).catch((error) => {
+    console.error('Error during Data Source initialization:', error)
+})
