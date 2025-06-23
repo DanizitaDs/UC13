295ada62da03df27f150862900a63119120e4c68
Danielle Yasmin Dias Padilha <162368805+DanizitaDs@users.noreply.github.com>
2025-06-23 17:21:02 -0300

    Add files via upload

---
+++ AtividadeAvaliativa/src/models/Livros.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert,BeforeUpdate, AfterLoad } from 'typeorm';
+
+@Entity('livro')
+export class Livro {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    name: string;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    tipo: string;
+
+    @Column({ type: "int", nullable: false }) // Corrigido para número
+    ano: number;
+
+    constructor(name: string, tipo: string, ano: number) {
+        this.name = name;
+        this.tipo = tipo;
+        this.ano = ano;
+    }
+}
